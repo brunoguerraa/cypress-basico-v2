a1671caddb92e408cfd505488b02282ceeca182b
brunoguerra23 <bruno.guerra@essentialnutrition.com.br>
2022-12-28 07:53:58 -0300

    teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -151,14 +151,5 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           })
     })
 
-    it('Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
-        cy.fixture('example.json').as('sampleFile')
-        cy.get('input[type="file"]')
-          .selectFile('@sampleFile')
-          .should(function($input) {
-            expect($input[0].files[0].name).to.equal('example.json')
-          })
-    })
-
   })
   
