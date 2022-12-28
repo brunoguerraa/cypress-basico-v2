3943d3ed0181cbffb68f40023606dae2a67b1235
brunoguerra23 <bruno.guerra@essentialnutrition.com.br>
2022-12-28 07:27:44 -0300

    teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -123,7 +123,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .uncheck()
           .should('not.be.checked')
     })
-
+ 
     it('Seleciona um arquivo da pasta fixtures', function() {
         cy.get('input[type="file"]')
           .should('not.have.value')
@@ -137,6 +137,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('input[type="file"]')
           .should('not.have.value')
           .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+          .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+          })
+    })
+
+    it('Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+          .selectFile('@sampleFile')
           .should(function($input) {
             expect($input[0].files[0].name).to.equal('example.json')
           })
